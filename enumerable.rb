54f0b1f8fea2b7ebe638c8429bf78ad2a7c418c8
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-08-26 16:44:30 -0400

    adding the my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -4,8 +4,23 @@ module Enumerable
             yield(i)
         end
     end
+
+    def my_each_with_index
+        count = self.length - 1
+        for num in self
+            index = self.find_index(num)
+            yield(num, index)
+            break if count <= 0
+            count -= 1
+        end
+    end
 end
 
-numbers = [2,4,7,9,1]
-result = numbers.my_each {|number| puts number}
+# numbers = [2,4,7,9,1]
+# result = numbers.my_each {|number| puts number}
+
+# numbers = [2,4,7,9,1]
+# result = numbers.my_each_with_index {|number, i| puts "number is #{number} and index is #{i}"}
+
+
 
