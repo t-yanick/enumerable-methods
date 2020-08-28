6fc256d121e99421b958964d6c00263e506c8790
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-08-28 13:54:54 -0400

    fixing some indentation eslinters and adding the disabling cop to file

--- enumerable.rb
+++ enumerable.rb
@@ -1,96 +1,98 @@
+# rubocop:disable Style/CaseEquality
+
 module Enumerable
-    def my_each
-        for i in self
-            yield(i) if block_given?
-        end
+  def my_each
+    for i in self
+      yield(i) if block_given?
     end
+  end
 
-    def my_each_with_index
-        count = self.length - 1
-        for num in self
-            index = self.find_index(num)
-            yield(num, index)
-            break if count <= 0
-            count -= 1
-        end
+  def my_each_with_index
+    count = self.length - 1
+    for num in self
+      index = self.find_index(num)
+      yield(num, index)
+      break if count <= 0
+      count -= 1
     end
+  end
 
-    def my_select
-        new_arr = []
-        self.my_each do |element|
-            result = yield(element)
-            if result == true
-                new_arr.push(element)
-            end
-        end
-        new_arr
+  def my_select
+    new_arr = []
+    self.my_each do |element|
+      result = yield(element)
+      if result == true
+        new_arr.push(element)
+      end
     end
+    new_arr
+  end
 
-    def my_all?
-        for item in self
-            if not yield(item)
-                return false
-            end
-        end
-        return true
-    end
-
-    def my_any?
-        for item in self
-            if yield(item)
-                return true
-            end
-        end
+  def my_all?
+    for item in self
+      if not yield(item)
         return false
+      end
     end
+    return true
+  end
 
-    def my_none?
-        for item in self
-            if yield(item)
-                return false
-            end
-        end
+  def my_any?
+    for item in self
+      if yield(item)
         return true
+      end
     end
+    return false
+  end
 
-    def my_count
-        new_arr = []
-        for item in self
-            if block_given?
-                if yield(item) == true
-                    new_arr.push(item)
-                    result = new_arr.length
-                else
-                    result = 0
-                end
-            else
-                result = self.length
-            end
+  def my_none?
+    for item in self
+      if yield(item)
+        return false
+      end
+    end
+    return true
+  end
+
+  def my_count
+    new_arr = []
+    for item in self
+      if block_given?
+        if yield(item) == true
+          new_arr.push(item)
+          result = new_arr.length
+        else
+          result = 0
         end
-        result
+      else
+        result = self.length
+      end
     end
+    result
+  end
 
-    def my_map (my_proc = false)
-        new_arr = []
-        for item in self
-            if block_given?
-                new_arr.push yield(item)
-            elsif my_proc 
-                new_arr.push my_proc.call(item) 
-            end 
-        end
-        new_arr
+  def my_map (my_proc = false)
+    new_arr = []
+    for item in self
+      if block_given?
+        new_arr.push yield(item)
+      elsif my_proc 
+        new_arr.push my_proc.call(item) 
+      end 
     end
+    new_arr
+  end
 
-    def my_inject
-        i = 0
-        accumulator = self[0]
-       while i < self.length - 1
-            accumulator = yield(accumulator, self[i+1])
-            i += 1
-       end
-       accumulator
+  def my_inject
+    i = 0
+    accumulator = self[0]
+    while i < self.length - 1
+      accumulator = yield(accumulator, self[i+1])
+      i += 1
     end
+    accumulator
+  end
 end
 
 def multiply_els(arr)
