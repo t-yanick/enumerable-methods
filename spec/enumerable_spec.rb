f2fde8bb331d076c69ea961d9be065aff3e3d86e
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-10-01 10:44:48 -0400

    Added my_each, my_each_with_index and my_select rspec tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -5,16 +5,14 @@ describe Enumerable do
   describe '#my_each' do
     let(:new_arr) { [2, 6, 8] }
 
-    it 'Should print the board' do
+    it 'Iterate over the array is called and do the instructions in the block predicate over each element' do
       expect do
         new_arr.my_each { |el| puts el + 2}
       end.to output("4\n8\n10\n").to_stdout
     end
 
-    it 'Should iterate and do the the instructions in the block of code over each element of the array that is called from' do
-      expect do
-        expect(new_arr.my_each { |el| el + 2}).yield_control
-      end
+    it 'Iterate over the array is called and yield control to the block predicate' do
+      expect(new_arr.my_each { |el| el + 2}).yield_self
     end
 
   end
@@ -22,15 +20,29 @@ describe Enumerable do
   describe '#my_each_with_index' do
     let(:new_arr) { [2, 6, 8] }
 
-    it 'Should iterate and do the instructions in the block of code over each element of the array that is called from' do
+    it 'Iterate over the array is called, do the instructions in the block predicate over each element and returns an the elements changed based in the instructions of the block predicate' do
       expect do
       new_arr.my_each_with_index { |el, i| puts el + i}
       end.to output("2\n7\n10\n").to_stdout
     end
 
-    it 'Should iterate and do the the instructions in the block of code over each element of the array that is called from' do
+    it 'Iterate over the array is called and yield control to the block predicate' do
+      expect(new_arr.my_each_with_index { |el| el + 2}).yield_self
+    end
+
+  end
+
+  describe '#my_select' do
+    let(:new_arr) { [1,2,3,4,5] }
+
+    it 'Iterate over the array is called, do the instructions in the block predicate over each element and return an array with the elements that comply with the block predicate' do
+      result = [1,2,3,4,5].my_select { |num| num.even? }
+      expect(result).to eq([2,4])
+    end
+
+    it 'Iterate over the array is called and yield control to the block predicate' do
       expect do
-        expect(new_arr.my_each_with_index { |el| el + 2}).yield_control
+        expect(new_arr.my_select { |num| num.even? }).yield_self
       end
     end
 
